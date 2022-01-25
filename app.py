60d2ece91fab94beb17b73dd535ca55f39ef2aa0
Caroline Knote <cj.knote@gmail.com>
2022-01-24 16:46:49 -0800

    adding session storage

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -10,7 +10,14 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 survey = satisfaction_survey
-responses = []
+
+@app.route('/surveystart', methods=["POST"])
+def start_survey():
+    """survey starting, setting session memory and redirects to first page"""
+    session["responses"] = []
+
+    #send to first question page
+    return redirect('/questions/0')
 
 @app.route('/')
 def start_page():
@@ -20,6 +27,7 @@ def start_page():
 @app.route('/questions/<int:num>')
 def question_page(num):
     """shows question passed in"""
+    responses = session['responses']
 
     #prevent users from skipping questions/going to questions that do not exist
     if(num!=len(responses)):
@@ -38,7 +46,10 @@ def store_answer():
     """store answer into the response memory"""
     user_answer = request.form['ans']
 
+    #update the session storage
+    responses = session.get('responses')
     responses.append(user_answer)
+    session['responses'] = responses
 
     #check if we still have questions to answer
     if(len(responses) == len(survey.questions)):
